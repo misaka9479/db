1c490e8b63b20789574f730b240d54ea1cdcd2ab
misaka9479 <you@example.com>
2019-04-28 09:50:37 +0800

    04271552

--- FACTORS.py
+++ FACTORS.py
@@ -8,10 +8,11 @@ import pandas as pd
 from pymongo import MongoClient
 
 if __name__ == '__main__':
-    client_raw = MongoClient(host='139.199.125.235', port=8888)
+    client_db = MongoClient(host='139.199.125.235', port=8888)
     client_factors = MongoClient(host='139.199.125.235', port=9999)
 
-    date = '20190422'  # date将在实际运行时由代码生成
+    date = '20190422'
+    codes_stock = client_db['STOCK'].list_collection_names()
 
     '''
     52-week high
@@ -19,10 +20,15 @@ if __name__ == '__main__':
     '''
     f_DATE_gte = (datetime.datetime.strptime(date, '%Y%m%d').date() - datetime.timedelta(52 * 7)).strftime('%Y%m%d')
     f_DATE_lte = date
-    data = client_raw['行情指标']['前收盘价'].find({'DATE': {'$gte': f_DATE_gte, '$lte': f_DATE_lte}})
+    data = []
+    for code in codes_stock:
+        foo = list(client_db['STOCK'][code].find({'DATE': {'$gte': f_DATE_gte, '$lte': f_DATE_lte}, 'NAME': '收盘价'}))
+        for i in foo:
+            i['CODE'] = code
+            data.append(i)
     data = pd.DataFrame(data)
     data['VALUE'] = data['VALUE'].astype(float)
-    data_max = data.groupby('CODE1')['VALUE'].max()
-    data_today = data[data['DATE'] == date].set_index('CODE1')['VALUE']
-    data = data_today / data_max
-    client_factors['FINANCIAL']['52WH'].insert_many([{'CODE1': CODE1, 'VALUE': str(VALUE), 'DATE': date} for CODE1, VALUE in data.items()])
+    data_max = data.groupby('CODE')['VALUE'].max()
+    data_today = data[data['DATE'] == date].set_index('CODE')['VALUE']
+    factor = data_today / data_max
+    client_factors['FINANCIAL']['52WH'].insert_many([{'CODE': CODE, 'VALUE': str(VALUE), 'DATE': date} for CODE, VALUE in factor.items()])
