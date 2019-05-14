04c7c8d5136f9967de2db88716bad74a495af614
misaka9479 <you@example.com>
2019-05-14 10:12:20 +0800

    50111028

--- STOCK.py
+++ STOCK.py
@@ -106,12 +106,49 @@ if __name__ == '__main__':
     # ////////// 股本指标
     # ////////// 股东指标
     # ////////// 行情指标
+
     # ////////// 估值指标
     # /// 总市值1
     get(codes, 'ev', 'unit=1;tradeDate={}'.format(date), '总市值1')
 
+    # # ///每股收益
+    get(codes, "eps_ttm", "tradeDate={}".format(date), '每股收益')
+
+    # # ///市现率PCF(现金净流量)
+    get(codes, "pcf_ncf", "tradeDate={};ruleType=2".format(date), '市现率PCF(现金净流量)', '上年年报')
+
+    # # ///市现率PCF(经营现金流)
+    get(codes, "pcf_ocf", "tradeDate={};ruleType=2".format(date), '市现率PCF(经营现金流)', '上年年报')
+
+    # # ///市盈率
+    # get(codes, "qstmnote_insur_212530", "unit=1;rptDate={}".format(date), '市盈率')
+
+    # # ///股息率(报告期)
+    get(codes, "dividendyield", "tradeDate={};rptYear=2018".format(date), '股息率(报告期)', '2018')  # todo
+
+    # /// 研发支出合计
+    get(codes, "researchanddevelopmentexpenses", "unit=1;rptDate={}".format(date), '研发支出合计')
+
+    # # ///净营运资本
+    get(codes, "networkingcapital", "unit=1;rptDate={}".format(date), '净营运资本')
+
+    # # /// 基本每股收益
+    get(codes, "eps_basic_is", "rptDate={};rptType=1".format(date), '基本每股收益 ', '合并报表')
+
     # ////////// 风险分析
+    # # /// BETA值(最近100周)
+    get(codes, "beta_100w", "tradeDate={}".format(date), 'BETA值(最近100周)')
+
     # ////////// 盈利预测
+    # # ///一致预测每股收益(FY1)
+    get(codes, "west_eps_FY1", "tradeDate={}".format(date), '一致预测每股收益(FY1)')
+
+    # # ///一致预测每股收益(FY2)
+    get(codes, "west_eps_FY2", "tradeDate={}".format(date), '一致预测每股收益(FY2)')
+
+    # # ///一致预测每股收益(FY3)
+    get(codes, "west_eps_FY3", "tradeDate={}".format(date), '一致预测每股收益(FY3)')
+
     # ////////// 财务分析
     # /// EBIT
     get(codes, 'ebit', 'unit=1;rptDate={}'.format(date), 'EBIT')
@@ -121,6 +158,12 @@ if __name__ == '__main__':
 
     # /// 净资产收益率ROE
     get(codes, 'roe', 'rptDate={}'.format(date), '净资产收益率ROE')
+
+    # # /// 净现金流
+    # get(codes, "qstmnote_insur_212530", "unit=1;rptDate={}".format(date), '净现金流')
+
+    # # /// 每股现金流量净额
+    get(codes, "cfps", "rptDate={};currencyType=".format(date), '每股现金流量净额')
 
     # ////////// 财务报表
 
@@ -202,6 +245,33 @@ if __name__ == '__main__':
     # /// 净利润
     get(codes, 'net_profit_is', 'unit=1;rptDate={};rptType=1'.format(date), '净利润', '合并报表')
 
+    # # /// 单季度净利润
+    get(codes, 'wgsd_qfa_nogaapprofit', 'unit=1;rptDate={};rptType=1;currencyType='.format(date), '单季度净利润', '合并报表')
+
+    # # ///投资活动现金流出小计
+    get(codes, "stot_cash_outflows_inv_act", "unit=1;rptDate={};rptType=1".format(date), '投资活动现金流出小计')
+
+    # # /// 销售费用
+    get(codes, "operateexpense_ttm2", "unit=1;rptDate={}".format(date), '销售费用', '合并报表')
+
+    # # /// 资产总计
+    get(codes, "tot_assets", "unit=1;rptDate={};rptType=1".format(date), '资产总计', '合并报表')
+
+    # # ///所有者权益合计
+    get(codes, "tot_equity", "unit=1;rptDate={};rptType=1".format(date), '所有者权益合计', '合并报表')
+
+    # # ///营业总收入
+    get(codes, "tot_oper_rev", "unit=1;rptDate={};rptType=1".format(date), '营业总收入', '合并报表')
+
+    # # /// 货币资金
+    get(codes, "monetary_cap", "unit=1;rptDate={};rptType=1".format(date), '货币资金', '合并报表')
+
+    # # /// 交易性金融资产
+    get(codes, "wgsd_invest_trading", "unit=1;rptDate={};rptType=1;currencyType=".format(date), '交易性金融资产', '合并报表')
+
+    # # ///总资产
+    get(codes, "wgsd_assets", "unit=1;rptDate={};rptType=1;currencyType=".format(date), '总资产', '合并报表')
+
     # ////////// 报表附注
     # /// 持有至到期投资减值损失
     get(codes, 'stmnote_ImpairmentLoss_9', 'unit=1;rptDate={};rptType=1'.format(date), '持有至到期投资减值损失', '合并报表')
@@ -221,8 +291,23 @@ if __name__ == '__main__':
     # /// 长期投资减值准备合计
     get(codes, 'stmnote_reserve_18', 'unit=1;rptDate={};rptType=1'.format(date), '持有至到期投资减值损失', '合并报表', '期末数')
 
+    # # /// 广告宣传推广费(销售费用)
+    get(codes, "stmnote_others_7633", "unit=1;rptDate={};rptType=1".format(date), '广告宣传推广费(销售费用)', '合并报表')
+
+    # # ///商誉减值损失
+    get(codes, "stmnote_ImpairmentLoss_6", "unit=1;rptDate={};rptType=1".format(date), '商誉减值损失', '合并报表')
+
     # ////////// 分红指标
     # ////////// 首发指标
+    # # ///发行数量合计
+    get(codes, "ipo_amount", "unit=1", '发行数量合计')
+
+    # # ///新股发行数量
+    get(codes, "ipo_newshares", "unit=1", '新股发行数量 ')
+
+    # # ///首发上市日期
+    get(codes, "ipo_date", '首发上市日期')
+
     # ////////// 增发指标
     # /// 增发上市日
     get(codes, 'fellow_listeddate', 'year=2018', '增发上市日', flag=False)
@@ -235,6 +320,18 @@ if __name__ == '__main__':
     # ////////// 股权分置改革
     # ////////// 技术形态
     # ////////// 其他指标
+
+    # # /// 增长率—利润总额(TTM)_PIT
+    get(codes, 'fa_tpgr_ttm', 'tradeDate={}'.format(date), '增长率—利润总额(TTM)_PIT')
+
+    # # /// 增长率—净利润(TTM)_PIT
+    get(codes, 'fa_npgr_ttm', 'tradeDate={}'.format(date), '增长率—净利润(TTM)_PIT')
+
+    # # /// 20日收益方差_PIT
+    get(codes, "risk_variance20", 'tradeDate={}'.format(date), '20日收益方差_PIT')
+
+    # # ///现金流资产比—资产回报率(TTM)_PIT
+    get(codes, "fa_acca_ttm", 'tradeDate={}'.format(date), '现金流资产比—资产回报率(TTM)_PIT')
 
     for code, docs in data_dict.items():
         client['STOCK'][code].insert_many(docs)
